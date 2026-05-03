4c3164abc8ab4d31c956461b4bb35ed9c5a67d82
KLAUSS Samuel <samuel.klauss@ynov.com>
2026-05-03 18:38:09 +0200

    refactor: moved scripts into the main containerfile

--- build/modules/packages/zed.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ouex pipefail
-
-echo "::group:: === Installing the Zed Package through terra ==="
-
-dnf5 install -y zed
-
-echo "::endgroup:: === Installation completed ==="
